b8bf10bac8bf1950a6277b0fb51bce466ba8ade0
Arthurchkeee <arthurchkeee@gmail.com>
2025-05-11 17:28:33 +0300

    fix 02-insert-data.sql

--- initdb/02-insert-data.sql
+++ initdb/02-insert-data.sql
@@ -6,3 +6,8 @@ INSERT INTO public.phone_data (id, phone, user_id) VALUES (1, '79207865432', 1);
 INSERT INTO public.phone_data (id, phone, user_id) VALUES (2, '79207865433', 2);
 INSERT INTO public.email_data (id, email, user_id) VALUES (1, 'JohnJohnson@gmail.com', 1);
 INSERT INTO public.email_data (id, email, user_id) VALUES (2, 'JohnKing@gmail.com', 2);
+
+
+ALTER SEQUENCE phone_data_id_seq RESTART WITH 3;
+
+ALTER SEQUENCE email_data_id_seq RESTART WITH 3;
